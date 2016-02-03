12a38e89f4d25c71827cc1aca6a0d644503a4fd2
Cesar Del Solar <delsolar@gmail.com>
2016-02-02 20:55:17 -0800

    small refactor

--- anagrammer/anagrammer.go
+++ anagrammer/anagrammer.go
@@ -2,7 +2,7 @@ package anagrammer
 
 import (
 	"github.com/domino14/macondo/gaddag"
-	"log"
+	_ "log"
 )
 
 const BlankPos = 31
@@ -22,52 +22,50 @@ func Anagram(letters string, dawg gaddag.SimpleDawg) []string {
 			rack[BlankPos] += 1
 		}
 	}
-	log.Println("Calling with rack", rack)
-	go anagram(rack, gd, gd.GetRootNodeIndex(), answerChan, "", alphabet)
+	go func() {
+		anagram(rack, gd, gd.GetRootNodeIndex(), answerChan, "", alphabet)
+		close(answerChan)
+	}()
 
 	for answer := range answerChan {
-		log.Println(answer)
 		answers = append(answers, answer)
 	}
-	log.Println("Peacing out")
 	return answers
+}
+
+func anagramHelper(letterCode byte, dawg gaddag.SimpleGaddag,
+	alphabet *gaddag.Alphabet, nodeIdx uint32, answers chan string,
+	rack []uint8, answerSoFar string) {
+
+	var nextNodeIdx uint32
+	var nextLetter rune
+	letter := alphabet.Letter(letterCode)
+	//log.Println(prefix, "Rack is now", rack, "letter", string(letter))
+	if dawg.InLetterSet(letter, nodeIdx) {
+		//	log.Println(prefix, string(letter), "in letter set")
+		answers <- answerSoFar + string(letter)
+	}
+
+	numArcs := dawg.NumArcs(nodeIdx)
+	for i := byte(1); i <= numArcs; i++ {
+		nextNodeIdx, nextLetter = dawg.ArcToIdxLetter(nodeIdx + uint32(i))
+		if letter == nextLetter {
+			anagram(rack, dawg, nextNodeIdx, answers,
+				answerSoFar+string(letter), alphabet)
+		}
+	}
 }
 
 func anagram(rack []uint8, dawg gaddag.SimpleGaddag, nodeIdx uint32,
 	answers chan string, answerSoFar string, alphabet *gaddag.Alphabet) {
 
-	var nextNodeIdx uint32
-	var nextLetter rune
-	prefix := ""
-	for i := 0; i < len([]rune(answerSoFar)); i++ {
-		prefix = prefix + " "
-	}
-	noTile := true
 	for idx, val := range rack {
 		if val == 0 {
 			continue
 		}
-		noTile = false
 		rack[idx] -= 1
-
-		letter := alphabet.Letter(byte(idx))
-		//log.Println(prefix, "Rack is now", rack, "letter", string(letter))
-		if dawg.InLetterSet(letter, nodeIdx) {
-			//	log.Println(prefix, string(letter), "in letter set")
-			answers <- answerSoFar + string(letter)
-		}
-
-		numArcs := dawg.NumArcs(nodeIdx)
-		for i := byte(1); i <= numArcs; i++ {
-			nextNodeIdx, nextLetter = dawg.ArcToIdxLetter(nodeIdx + uint32(i))
-			if letter == nextLetter {
-				anagram(rack, dawg, nextNodeIdx, answers,
-					answerSoFar+string(letter), alphabet)
-			}
-		}
+		anagramHelper(byte(idx), dawg, alphabet, nodeIdx, answers, rack,
+			answerSoFar)
 		rack[idx] += 1
-	}
-	if noTile {
-		//close(answers)
 	}
 }
--- anagrammer/anagrammer_test.go
+++ anagrammer/anagrammer_test.go
@@ -3,9 +3,34 @@ package anagrammer
 import "testing"
 import "github.com/domino14/macondo/gaddag"
 
+type testpair struct {
+	rack string
+	num  int
+}
+
+var buildTests = []testpair{
+	{"AEHILORT", 269},
+	{"CINEMATOGRAPHER", 3015},
+	{"KERYGMA", 88},
+	{"LOCOFOCO", 14},
+	{"VIVIFIC", 2},
+	{"ZYZZYVA", 6},
+	{"HHHHHHHH", 0},
+	{"OCTOROON", 34},
+	// {"FIREFANG????", 53618},
+	// {"AEINST??", 9246},
+	// {"ZZZZ?", 3},
+	// {"???", 1116},
+}
+
 func TestAnagram(t *testing.T) {
 	gaddag.GenerateDawg("/Users/cesar/coding/webolith/words/OWL2.txt", true,
 		true)
-	d := gaddag.LoadGaddag("out.dawg")
-	Anagram("AEROLITH", gaddag.SimpleDawg(d))
+	d := gaddag.SimpleDawg(gaddag.LoadGaddag("out.dawg"))
+	for _, pair := range buildTests {
+		answers := Anagram(pair.rack, d)
+		if len(answers) != pair.num {
+			t.Error("For", pair.rack, "expected", pair.num, "got", answers)
+		}
+	}
 }
