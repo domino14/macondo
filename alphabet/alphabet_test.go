bad106aa41917f01c1360091dcb4c18044bb62b9
Cesar Del Solar <delsolar@gmail.com>
2018-11-28 00:05:04 -0500

    oops, fix a teensy test

--- alphabet/alphabet_test.go
+++ alphabet/alphabet_test.go
@@ -19,13 +19,13 @@ func TestUserVisible(t *testing.T) {
 	if !reflect.DeepEqual(alph.letterSlice, expected) {
 		t.Errorf("Did not equal, expected %v got %v", expected, alph.letterSlice)
 	}
-	mw := MachineWord([]rune{4, 8, 7, 5, 2})
+	mw := MachineWord([]MachineLetter{4, 8, 7, 5, 2})
 	uv := mw.UserVisible(alph)
 	if uv != "HOMIE" {
 		t.Errorf("Did not equal, expected %v got %v", "HOMIE", uv)
 	}
 
-	mw2 := MachineWord([]rune{12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0})
+	mw2 := MachineWord([]MachineLetter{12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0})
 	uv2 := mw2.UserVisible(alph)
 	if uv2 != "XUTROMLIHGEDA" {
 		t.Errorf("Did not equal, expected %v got %v", "XUTROMLIHGEDA", uv2)
