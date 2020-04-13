3961db0c2ccf056e9862e7495cfd2bbc584b62d9
Cesar Del Solar <delsolar@gmail.com>
2020-04-13 00:27:32 -0400

    further progress on rpc api stuff

--- game/game.go
+++ game/game.go
@@ -3,11 +3,57 @@
 package game
 
 import (
+	"github.com/domino14/macondo/alphabet"
+	"github.com/domino14/macondo/board"
+	"github.com/domino14/macondo/config"
+	"github.com/domino14/macondo/gaddag"
 	"github.com/domino14/macondo/move"
 	pb "github.com/domino14/macondo/rpc/api/proto"
+	"github.com/lithammer/shortuuid"
 )
 
+// // History is a wrapper around pb.GameHistory to allow for additional
+// // methods and an actual array of positions.
+// type History struct {
+// 	pb.GameHistory
+
+// }
+
+// Position is a wrapper around pb.GamePosition that has additional
+// methods and values associated with it, such as the current board state.
+type Position struct {
+	pb.GamePosition
+
+	board *board.GameBoard
+	bag   *alphabet.Bag
+	// The pb.GamePosition doesn't know how to deal with alphabet.Rack,
+	// so we add an extra field here just for the alphabet rack,
+	// mapped 1-to-1 to the pb.GamePosition.Players array.
+	playerRacks []*alphabet.Rack
+}
+
+// Game is the actual internal game structure that controls the entire
+// business logic of the game; drawing, making moves, etc. The two
+// structures above are basically data entities.
+// Note: a Game doesn't care how it is played. It is just rules for gameplay.
+// AI players, human players, etc will play a game outside of the scope of
+// this module.
+type Game struct {
+	gaddag  *gaddag.SimpleGaddag
+	alph    *alphabet.Alphabet
+	playing bool
+
+	history *pb.GameHistory
+	// An array of Position, corresponding to the internal protobuf array
+	// of pb.GameTurn.
+	positions []*Position
+}
+
+// CalculateCoordsFromStringPosition turns a "position" on the board such as
+// H7 and turns it into a numeric row, col, and direction.
 func CalculateCoordsFromStringPosition(evt *pb.GameEvent) {
+	// Note that this evt.Position has nothing to do with the type Position
+	// we are defining in this package.
 	row, col, vertical := move.FromBoardGameCoords(evt.Position)
 	if vertical {
 		evt.Direction = pb.GameEvent_VERTICAL
@@ -16,4 +62,32 @@ func CalculateCoordsFromStringPosition(evt *pb.GameEvent) {
 	}
 	evt.Row = int32(row)
 	evt.Column = int32(col)
+}
+
+func newHistory(players []*pb.PlayerInfo) *History {
+	his := &pb.GameHistory{}
+	his.Players = players
+	his.Uuid = shortuuid.New()
+	return his
+}
+
+// NewGame is how one instantiates a brand new game. It instantiates a history,
+// and deals tiles, creating the first position in the history as well.
+func NewGame(cfg *config.Config, players []*pb.PlayerInfo, boardLayout []string,
+	lexiconName string, letterDistributionName string) (*Game, error) {
+
+	game := &Game{}
+	game.history = newHistory(players)
+
+	// Initialize a new position.
+	position := &Position{}
+
+	// 0. create a random seed?
+	// 1. pick a random player to go first.
+	// 2. set position onturn, turnnum, playing, board
+	// 3. deal out tiles, set position bag to have 86 tiles in it
+	// 4. set position playerRacks to be the player racks, and the position
+	//   internal pb struct for the racks to be the string repr of these
+	// 5. add position to game.positions
+	// 6. return game.
 }
--- game/rpc_service.go
+++ game/rpc_service.go
@@ -2,15 +2,53 @@ package game
 
 import (
 	"context"
+	"os"
+	"time"
 
+	"github.com/domino14/macondo/config"
+	"github.com/domino14/macondo/endgame/alphabeta"
+	"github.com/domino14/macondo/montecarlo"
+	"github.com/domino14/macondo/movegen"
 	pb "github.com/domino14/macondo/rpc/api/proto"
 )
 
+// Note that we will have some singleton data structures here, even though
+// this is a server. It is not meant to have multiple games loaded in memory
+// at once. If we want to make this a part of a general Crossword PvP tool,
+// we should only import the relevant packages, but handle all the database
+// stuff in the main PvP tool.
+
 // AnnotationService will be the main API that the front end will talk to
-// to annotate, simulate, etc. a game.
-type AnnotationService struct{}
+// to annotate, simulate, etc. a game. Note that the variables here are singletons
+// for the whole service, as in the comment above.
+// All of the methods of this service should be very thin wrappers so that the
+// meat of the code is as reusable as possible.
+type AnnotationService struct {
+	game *Game
+	cfg  *config.Config
+	// Simmer:
+	simmer        *montecarlo.Simmer
+	simCtx        context.Context
+	simCancel     context.CancelFunc
+	simTicker     *time.Ticker
+	simTickerDone chan bool
+	simLogFile    *os.File
+
+	gen           movegen.MoveGenerator
+	endgameSolver *alphabeta.Solver
+}
+
+func NewAnnotationService(cfg *config.Config) *AnnotationService {
+	return &AnnotationService{cfg: cfg}
+}
 
 func (a *AnnotationService) NewGame(ctx context.Context, gameReq *pb.NewGameRequest) (*pb.GameHistory, error) {
 	// log := zerolog.Ctx(ctx)
-	return nil, nil
+	var err error
+	a.game, err = NewGame(a.cfg, gameReq.Players, gameReq.BoardLayout, gameReq.Lexicon,
+		gameReq.LetterDistribution)
+	if err != nil {
+		return nil, err
+	}
+	return a.game.history, nil
 }
