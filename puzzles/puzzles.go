9a6f8eb9e749a8c58a85aaff658124a8145f21b5
jvc56 <jvc56@cornell.edu>
2022-04-07 20:15:59 -0400

    Add more puzzle tags

--- puzzles/puzzles.go
+++ puzzles/puzzles.go
@@ -2,15 +2,25 @@ package puzzles
 
 import (
 	"github.com/domino14/macondo/ai/runner"
+	"github.com/domino14/macondo/alphabet"
 	"github.com/domino14/macondo/config"
+	"github.com/domino14/macondo/gaddag"
 	"github.com/domino14/macondo/game"
 	pb "github.com/domino14/macondo/gen/api/proto/macondo"
 	"github.com/domino14/macondo/move"
+
+	"github.com/rs/zerolog/log"
 )
 
-var PuzzleFunctions = []func(moves []*move.Move) (bool, pb.PuzzleTag){
+var PuzzleFunctions = []func(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag){
 	EquityPuzzle,
+	BingoPuzzle,
 	OnlyBingoPuzzle,
+	BlankBingoPuzzle,
+	NonBingoPuzzle,
+	PowerTilePuzzle,
+	BingoNineOrAbovePuzzle,
+	CELOnlyPuzzle,
 }
 
 func CreatePuzzlesFromGame(conf *config.Config, g *game.Game) ([]*pb.PuzzleCreationResponse, error) {
@@ -39,7 +49,7 @@ func CreatePuzzlesFromGame(conf *config.Config, g *game.Game) ([]*pb.PuzzleCreat
 		turnIsPuzzle := false
 		tags := []pb.PuzzleTag{}
 		for _, puzzleFunc := range PuzzleFunctions {
-			turnIsPuzzleType, tag := puzzleFunc(moves)
+			turnIsPuzzleType, tag := puzzleFunc(g, moves)
 			turnIsPuzzle = turnIsPuzzle || turnIsPuzzleType
 			if turnIsPuzzleType {
 				tags = append(tags, tag)
@@ -56,19 +66,135 @@ func CreatePuzzlesFromGame(conf *config.Config, g *game.Game) ([]*pb.PuzzleCreat
 	return puzzles, nil
 }
 
-func EquityPuzzle(moves []*move.Move) (bool, pb.PuzzleTag) {
+func EquityPuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
 	return len(moves) >= 2 && moves[0].Equity() > moves[1].Equity()+10, pb.PuzzleTag_EQUITY
 }
 
-func OnlyBingoPuzzle(moves []*move.Move) (bool, pb.PuzzleTag) {
+func BingoPuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
+	m := moves[0]
+	return moveIsBingo(m), pb.PuzzleTag_BINGO
+}
+
+func OnlyBingoPuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
 	tag := pb.PuzzleTag_ONLY_BINGO
-	if len(moves) == 0 || moves[0].TilesPlayed() != 7 {
+	if len(moves) == 0 || !moveIsBingo(moves[0]) {
 		return false, tag
 	}
-	for _, mv := range moves[1:] {
-		if mv.TilesPlayed() == 7 {
+	for _, m := range moves[1:] {
+		if moveIsBingo(m) && m.Action() == move.MoveTypePlay {
 			return false, tag
 		}
 	}
 	return true, tag
+}
+
+func BlankBingoPuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
+	m := moves[0]
+	return moveIsBingo(m) && moveContainsBlank(m), pb.PuzzleTag_BLANK_BINGO
+}
+
+func NonBingoPuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
+	return !moveIsBingo(moves[0]), pb.PuzzleTag_NON_BINGO
+}
+
+// XXX: Must be expanded to other languages
+func PowerTilePuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
+	return moveContainsLetter(moves[0], "XJZQ"), pb.PuzzleTag_POWER_TILE
+}
+
+func BingoNineOrAbovePuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
+	m := moves[0]
+	return moveIsBingo(m) && moveLength(m) >= 9, pb.PuzzleTag_BINGO_NINE_OR_ABOVE
+}
+
+func CELOnlyPuzzle(g *game.Game, moves []*move.Move) (bool, pb.PuzzleTag) {
+	m := moves[0]
+	evt := g.EventFromMove(m)
+	wordsFormed, err := g.ValidateMove(m)
+	if err != nil {
+		log.Debug().Err(err).Msg("cel-only-validation-error")
+		return false, pb.PuzzleTag_CEL_ONLY
+	}
+	evt.WordsFormed = convertToVisible(wordsFormed, g.Alphabet())
+	isCEL, err := isCELEvent(evt, g.History(), g.Config())
+	if err != nil {
+		log.Debug().Err(err).Msg("cel-only-phony-error")
+		return false, pb.PuzzleTag_CEL_ONLY
+	}
+	return isCEL, pb.PuzzleTag_CEL_ONLY
+}
+
+func moveLength(m *move.Move) int {
+	return len(m.Tiles())
+}
+
+func moveIsBingo(m *move.Move) bool {
+	return m.TilesPlayed() == 7
+}
+
+func moveContainsBlank(m *move.Move) bool {
+	for _, ml := range m.Tiles() {
+		if ml >= alphabet.BlankOffset {
+			return true
+		}
+	}
+	return false
+}
+
+func moveContainsLetter(m *move.Move, letters string) bool {
+	mls, err := alphabet.ToMachineLetters(letters, m.Alphabet())
+	if err != nil {
+		log.Debug().Err(err).Msg("move-contains-letters-error")
+		return false
+	}
+	mlm := map[alphabet.MachineLetter]bool{}
+	for _, ml := range mls {
+		mlm[ml] = true
+	}
+	for _, moveTile := range m.Tiles() {
+		if mlm[moveTile] {
+			return true
+		}
+	}
+	return false
+}
+
+func isCELEvent(event *pb.GameEvent, history *pb.GameHistory, cfg *config.Config) (bool, error) {
+	dawg, err := gaddag.GetDawg(cfg, "ECWL")
+	if err != nil {
+		return false, err
+	}
+	for _, word := range event.WordsFormed {
+		phony, err := isPhony(dawg, word, history.Variant)
+		if err != nil || phony {
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func isPhony(gd gaddag.GenericDawg, word, variant string) (bool, error) {
+	lex := gaddag.Lexicon{GenericDawg: gd}
+	machineWord, err := alphabet.ToMachineWord(word, lex.GetAlphabet())
+	if err != nil {
+		return false, err
+	}
+	var valid bool
+	switch string(variant) {
+	case string(game.VarWordSmog):
+		valid = lex.HasAnagram(machineWord)
+	default:
+		valid = lex.HasWord(machineWord)
+	}
+	return !valid, nil
+}
+
+func convertToVisible(words []alphabet.MachineWord,
+	alph *alphabet.Alphabet) []string {
+
+	uvstrs := make([]string, len(words))
+	for idx, w := range words {
+		uvstrs[idx] = w.UserVisible(alph)
+	}
+	return uvstrs
 }
--- puzzles/puzzles_test.go
+++ puzzles/puzzles_test.go
@@ -1,26 +1,27 @@
 package puzzles
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"testing"
 
-	"github.com/domino14/macondo/alphabet"
 	"github.com/domino14/macondo/board"
 	"github.com/domino14/macondo/config"
 	"github.com/domino14/macondo/gaddagmaker"
 	"github.com/domino14/macondo/game"
 	"github.com/domino14/macondo/gcgio"
 	pb "github.com/domino14/macondo/gen/api/proto/macondo"
-	"github.com/domino14/macondo/move"
 	"github.com/matryer/is"
+	"github.com/rs/zerolog"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
 var DefaultConfig = config.DefaultConfig()
 
 func TestMain(m *testing.M) {
-	for _, lex := range []string{"CSW19"} {
+	for _, lex := range []string{"CSW21"} {
 		gdgPath := filepath.Join(DefaultConfig.LexiconPath, "gaddag", lex+".gaddag")
 		if _, err := os.Stat(gdgPath); os.IsNotExist(err) {
 			gaddagmaker.GenerateGaddag(filepath.Join(DefaultConfig.LexiconPath, lex+".txt"), true, true)
@@ -33,48 +34,80 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestBestEquity(t *testing.T) {
+func TestPuzzles(t *testing.T) {
 	is := is.New(t)
-	players := []*pb.PlayerInfo{
-		{Nickname: "JD", RealName: "Jesse"},
-		{Nickname: "cesar", RealName: "César"},
+	zerolog.SetGlobalLevel(zerolog.Disabled)
+	equityPuzzle := &pb.PuzzleCreationResponse{TurnNumber: 1,
+		Answer: &pb.GameEvent{
+			Type:        pb.GameEvent_TILE_PLACEMENT_MOVE,
+			Row:         0,
+			Column:      7,
+			Direction:   pb.GameEvent_VERTICAL,
+			PlayedTiles: "KOFTGAR.",
+		},
+		Tags: []pb.PuzzleTag{pb.PuzzleTag_BINGO, pb.PuzzleTag_EQUITY},
 	}
+	puzzlesMatch(is, "equity", equityPuzzle)
 
-	rules, err := game.NewBasicGameRules(
-		&DefaultConfig, "CSW19", board.CrosswordGameLayout, "english",
-		game.CrossScoreAndSet, game.VarClassic)
-	is.NoErr(err)
+	onlyBingoPuzzle := &pb.PuzzleCreationResponse{TurnNumber: 1,
+		Answer: &pb.GameEvent{
+			Type:        pb.GameEvent_TILE_PLACEMENT_MOVE,
+			Row:         7,
+			Column:      8,
+			Direction:   pb.GameEvent_VERTICAL,
+			PlayedTiles: ".EQUORIN",
+		},
+		Tags: []pb.PuzzleTag{pb.PuzzleTag_BINGO, pb.PuzzleTag_EQUITY, pb.PuzzleTag_ONLY_BINGO, pb.PuzzleTag_POWER_TILE},
+	}
+	puzzlesMatch(is, "only_bingo", onlyBingoPuzzle)
 
-	g, _ := game.NewGame(rules, players)
-	alph := g.Alphabet()
-	g.StartGame()
-	g.SetPlayerOnTurn(0)
+	blankBingoPuzzle := &pb.PuzzleCreationResponse{TurnNumber: 11,
+		Answer: &pb.GameEvent{
+			Type:        pb.GameEvent_TILE_PLACEMENT_MOVE,
+			Row:         2,
+			Column:      11,
+			Direction:   pb.GameEvent_VERTICAL,
+			PlayedTiles: "GLOBAtE",
+		},
+		Tags: []pb.PuzzleTag{pb.PuzzleTag_BINGO, pb.PuzzleTag_EQUITY, pb.PuzzleTag_BLANK_BINGO},
+	}
+	puzzlesMatch(is, "only_bingo", blankBingoPuzzle)
 
-	g.SetRackFor(0, alphabet.RackFromString("AEIQRST", alph))
-	m := move.NewScoringMoveSimple(22, "8G", "QI", "AERST", alph)
-	words, err := g.ValidateMove(m)
-	is.NoErr(err)
-	is.Equal(len(words), 1)
-	g.PlayMove(m, true, 0)
+	celOnlyPuzzle := &pb.PuzzleCreationResponse{TurnNumber: 5,
+		Answer: &pb.GameEvent{
+			Type:        pb.GameEvent_TILE_PLACEMENT_MOVE,
+			Row:         2,
+			Column:      2,
+			Direction:   pb.GameEvent_HORIZONTAL,
+			PlayedTiles: "ADMITS",
+		},
+		Tags: []pb.PuzzleTag{pb.PuzzleTag_CEL_ONLY, pb.PuzzleTag_NON_BINGO},
+	}
+	puzzlesMatch(is, "cel_only", celOnlyPuzzle)
 
-	g.SetRackFor(1, alphabet.RackFromString("AFGKORT", alph))
-	m = move.NewScoringMoveSimple(17, "9H", "FORK", "AGT", alph)
-	words, err = g.ValidateMove(m)
-	is.NoErr(err)
-	is.Equal(len(words), 2)
-	g.PlayMove(m, true, 0)
+	bingoNinePuzzle := &pb.PuzzleCreationResponse{TurnNumber: 2,
+		Answer: &pb.GameEvent{
+			Type:        pb.GameEvent_TILE_PLACEMENT_MOVE,
+			Row:         2,
+			Column:      7,
+			Direction:   pb.GameEvent_VERTICAL,
+			PlayedTiles: "WATER..OI",
+		},
+		Tags: []pb.PuzzleTag{pb.PuzzleTag_BINGO_NINE_OR_ABOVE, pb.PuzzleTag_BINGO, pb.PuzzleTag_EQUITY, pb.PuzzleTag_ONLY_BINGO},
+	}
+	puzzlesMatch(is, "bingo_nine_or_above", bingoNinePuzzle)
 
-	puzzles, err := CreatePuzzlesFromGame(&DefaultConfig, g)
-	is.NoErr(err)
-	is.Equal(len(puzzles), 1)
-	is.Equal(puzzles[0].TurnNumber, int32(1))
-	is.Equal(puzzles[0].GameId, g.Uid())
-	// a game move doesn't have WordsFormed, so need to look at the actual
-	// tiles played:
-	is.Equal(puzzles[0].Answer.PlayedTiles, "KOFTGAR.")
-	is.Equal(puzzles[0].Answer.Position, "H1")
-	is.Equal(len(puzzles[0].Tags), 1)
-	is.Equal(puzzles[0].Tags[0], pb.PuzzleTag_EQUITY)
+	bingoFifteenPuzzle := &pb.PuzzleCreationResponse{TurnNumber: 4,
+		Answer: &pb.GameEvent{
+			Type:        pb.GameEvent_TILE_PLACEMENT_MOVE,
+			Row:         2,
+			Column:      0,
+			Direction:   pb.GameEvent_HORIZONTAL,
+			PlayedTiles: "ELECTRO........",
+		},
+		Tags: []pb.PuzzleTag{pb.PuzzleTag_BINGO_NINE_OR_ABOVE, pb.PuzzleTag_BINGO, pb.PuzzleTag_EQUITY},
+	}
+	puzzlesMatch(is, "bingo_nine_or_above", bingoFifteenPuzzle)
 }
 
 func TestLostChallenge(t *testing.T) {
@@ -86,7 +119,7 @@ func TestLostChallenge(t *testing.T) {
 	// Set the correct challenge rule
 	gameHistory.ChallengeRule = pb.ChallengeRule_FIVE_POINT
 
-	rules, err := game.NewBasicGameRules(&DefaultConfig, "CSW19", board.CrosswordGameLayout, "english", game.CrossScoreAndSet, game.VarClassic)
+	rules, err := game.NewBasicGameRules(&DefaultConfig, "CSW21", board.CrosswordGameLayout, "english", game.CrossScoreAndSet, game.VarClassic)
 	is.NoErr(err)
 
 	game, err := game.NewFromHistory(gameHistory, rules, 0)
@@ -111,7 +144,56 @@ func TestPhonyTilesReturned(t *testing.T) {
 	game, err := game.NewFromHistory(gh, rules, 0)
 	is.NoErr(err)
 
-	pzls, err := CreatePuzzlesFromGame(&DefaultConfig, game)
-	is.Equal(len(pzls), 4)
+	_, err = CreatePuzzlesFromGame(&DefaultConfig, game)
 	is.NoErr(err)
+}
+
+func puzzlesMatch(is *is.I, gcgfile string, expectedPzl *pb.PuzzleCreationResponse) {
+	gameHistory, err := gcgio.ParseGCG(&DefaultConfig, fmt.Sprintf("./testdata/%s.gcg", gcgfile))
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the challenge rule to five point
+	// so GCGs with challenges will load
+	gameHistory.ChallengeRule = pb.ChallengeRule_FIVE_POINT
+
+	rules, err := game.NewBasicGameRules(&DefaultConfig, "CSW21", board.CrosswordGameLayout, "english", game.CrossScoreAndSet, game.VarClassic)
+	if err != nil {
+		panic(err)
+	}
+	game, err := game.NewFromHistory(gameHistory, rules, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	pzls, err := CreatePuzzlesFromGame(&DefaultConfig, game)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, pzl := range pzls {
+		if expectedPzl.TurnNumber == pzl.TurnNumber {
+			fmt.Printf("%v\n", pzl.Tags)
+			fmt.Printf("%v\n", pzl.Answer)
+			is.Equal(expectedPzl.Answer.Type, pzl.Answer.Type)
+			is.Equal(expectedPzl.Answer.Row, pzl.Answer.Row)
+			is.Equal(expectedPzl.Answer.Column, pzl.Answer.Column)
+			is.Equal(expectedPzl.Answer.Direction, pzl.Answer.Direction)
+			is.Equal(expectedPzl.Answer.PlayedTiles, pzl.Answer.PlayedTiles)
+
+			is.Equal(len(pzl.Tags), len(expectedPzl.Tags))
+			sortTags(pzl.Tags)
+			sortTags(expectedPzl.Tags)
+			for i := range pzl.Tags {
+				is.Equal(expectedPzl.Tags[i], pzl.Tags[i])
+			}
+		}
+	}
+}
+
+func sortTags(tags []pb.PuzzleTag) {
+	sort.Slice(tags, func(i, j int) bool {
+		return tags[i] < tags[j]
+	})
 }
