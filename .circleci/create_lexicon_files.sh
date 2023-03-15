9600cbc964cf3f427f4df3671e4044521cf8fcf3
César Del Solar <delsolar@gmail.com>
2023-03-14 23:06:49 -0400

    more logging

--- .circleci/create_lexicon_files.sh
+++ .circleci/create_lexicon_files.sh
@@ -2,6 +2,8 @@ set -e
 for lex in "NWL20" "NWL18" "America" "CSW21" "CSW19"
 do
     awk '{print $1}' $LEXICON_PATH/$lex.txt > $LEXICON_PATH/$lex-stripped.txt
+    echo "lex $lex"
+    ls -al $LEXICON_PATH
     docker run --rm -v $LEXICON_PATH:/opt kbuilder -- english-kwg /opt/$lex-stripped.txt /opt/gaddag/$lex.kwg
 done
 
