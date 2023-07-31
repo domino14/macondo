9f0f1733ba7497a190c2be4feef4caa61a6cd8d9
César Del Solar <delsolar@gmail.com>
2023-07-30 23:37:18 -0400

    update mac build

--- tasks.py
+++ tasks.py
@@ -8,12 +8,8 @@ def build(c):
     tag = c.run("git describe --exact-match --tags", hide=True).stdout.strip()
     print("Tag was", tag)
     c.run("go build -o macondo ./cmd/shell")
-    c.run("go build ./cmd/make_gaddag")
-    c.run("go build ./cmd/make_leaves_structure")
     c.run(
-        "tar --exclude './data/strategy/default_english/*.idx' "
-        "--exclude './data/letterdistributions' "
+        "tar --exclude './data/strategy_default_english/quackle*' "
         f"-czvf macondo-darwin-{tag}.tar.gz "
-        "./macondo ./make_gaddag ./make_leaves_structure ./data "
-        "./shell/helptext"
+        "./macondo ./data "
     )
