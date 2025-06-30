0c892520c4580a667cfa5fa1246ba9514e8bea24
César Del Solar <delsolar@gmail.com>
2025-06-29 23:57:33 -0400

    start on a magpie interface

--- shell/api.go
+++ shell/api.go
@@ -25,6 +25,7 @@ import (
 	"github.com/domino14/macondo/game"
 	"github.com/domino14/macondo/gcgio"
 	pb "github.com/domino14/macondo/gen/api/proto/macondo"
+	"github.com/domino14/macondo/magpie"
 	"github.com/domino14/macondo/move"
 	"github.com/domino14/macondo/movegen"
 	"github.com/domino14/macondo/preendgame"
@@ -962,6 +963,11 @@ func (sc *ShellController) winpct(cmd *shellcmd) (*Response, error) {
 
 	return msg(fmt.Sprintf("Win percentage: %.2f%%", wpct*100)), nil
 
+}
+
+func (sc *ShellController) magpie(cmd *shellcmd) (*Response, error) {
+	magpie.SanityTest()
+	return nil, nil
 }
 
 func (sc *ShellController) mleval(cmd *shellcmd) (*Response, error) {
