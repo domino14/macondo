5a0aa722a76b6634d52ae8eae3884c734d4aa15a
César Del Solar <delsolar@gmail.com>
2025-06-30 23:04:12 -0400

    capture magpie output into string

--- magpie/magpie.go
+++ magpie/magpie.go
@@ -5,29 +5,62 @@ package magpie
 #cgo LDFLAGS: -L../../magpie -lmagpie -lm
 #include "impl/wasm_api.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// Helper to flush C stdio buffers
+void flush_c_stdout() { fflush(stdout); }
 */
 import "C"
 import (
+	"bytes"
 	"fmt"
-	"time"
+	"os"
+	"syscall"
 	"unsafe"
 )
 
 func sendCmd(cmd string) {
 	cCmd := C.CString(cmd)
 	defer C.free(unsafe.Pointer(cCmd))
-	C.process_command_wasm(cCmd)
+	C.process_command_wasm_sync(cCmd)
 
 }
 
-func sendCmdWithPoll(cmd string) {
-	sendCmd(cmd)
-	time.Sleep(500 * time.Millisecond) // Give some time for the command to be processed
+// CaptureCStdout runs fn() and returns everything written to C stdout as a string.
+func CaptureCStdout(fn func()) string {
+	// Create a pipe
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
 
-	resp := C.get_search_status_wasm()
-	fmt.Println("Response from C:", C.GoString(resp))
+	// Save original stdout fd
+	origStdout, err := syscall.Dup(1)
+	if err != nil {
+		panic(err)
+	}
 
-	defer C.free(unsafe.Pointer(resp))
+	// Redirect stdout to pipe
+	syscall.Dup2(int(w.Fd()), 1)
+
+	// Run the function (which calls C code)
+	fn()
+
+	// Flush C stdio buffers
+	C.flush_c_stdout()
+
+	// Restore original stdout
+	w.Close()
+	syscall.Dup2(origStdout, 1)
+	syscall.Close(origStdout)
+
+	// Read output
+	var buf bytes.Buffer
+	_, _ = buf.ReadFrom(r)
+	r.Close()
+
+	return buf.String()
 }
 
 // SanityTest
@@ -38,12 +71,12 @@ func SanityTest() {
 	C.wasm_init_configs(cPaths)
 
 	sendCmd("set -s1 equity -s2 equity -r1 equity -r2 equity -numplays 100 -plies 5 -maxequitydiff 30 -sr tt -threads 16 -thres gk16 -scond 99 -it 1000000 -wmp true")
-	time.Sleep(500 * time.Millisecond) // Give some time for the command to be processed
 	sendCmd("cgp C14/O2TOY9/mIRADOR8/F4DAB2PUGH1/I5GOOEY3V/T4XI2MALTHA/14N/6GUM3OWN/7PEW2DOE/9EF1DOR/2KUNA1J1BEVELS/3TURRETs2S2/7A4T2/7N7/7S7 EEEIILZ/ 336/298 0 -lex NWL23")
-	time.Sleep(500 * time.Millisecond) // Give some time for the command to be processed
-
-	sendCmd("gen")
-	time.Sleep(500 * time.Millisecond) // Give some time for the command to be processed
-	fmt.Println("starting simulation")
-	sendCmd("sim")
+	CaptureCStdout(func() {
+		sendCmd("gen")
+	})
+	output := CaptureCStdout(func() {
+		sendCmd("sim")
+	})
+	fmt.Println("MAGPIE Simulation Output:", output)
 }
